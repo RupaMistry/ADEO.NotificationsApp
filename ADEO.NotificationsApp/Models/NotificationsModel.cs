e6e84858f01973142d04882e5af37c9b6022616f
RupaMistry <mistryrupa7@gmail.com>
2024-08-27 23:44:09 +0400

    Updated.

--- ADEO.NotificationsApp/Models/NotificationsModel.cs
+++ ADEO.NotificationsApp/Models/NotificationsModel.cs
@@ -7,14 +7,5 @@ namespace ADEO.NotificationsApp.Web.Models
         public IReadOnlyList<UserMessage> UserMessages { get; set; }
 
         public IReadOnlyList<UserMessage> MessageHistory { get; set; }
-    }
-
-    public class MessageCreationResponse
-    {
-        public bool IsSuccess { get; set; }
-
-        public string Error { get; set; } 
-
-        public NotificationsModel NotificationsModel { get; set; }
-    }
+    }   
 }
